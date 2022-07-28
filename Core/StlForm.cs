b47672ec27c4a5c3ebe67657aa51cf0c57c92e55
timlai <timlai99@outlook.com>
2022-07-28 19:28:29 +0800

    bugfix

--- Core/StlForm.cs
+++ Core/StlForm.cs
@@ -15,14 +15,17 @@ namespace SSCMS.Form.Core
         private const string AttributeTitle = "title";
         private const string AttributeName = "name";
         private const string AttributeType = "type";
+        private const string AttributeHeight = "height";
 
         private readonly IPathManager _pathManager;
+        private readonly IPluginManager _pluginManager;
         private readonly ISiteRepository _siteRepository;
         private readonly IFormRepository _formRepository;
 
-        public StlForm(IPathManager pathManager, ISiteRepository siteRepository, IFormRepository formRepository)
+        public StlForm(IPathManager pathManager, IPluginManager pluginManager, ISiteRepository siteRepository, IFormRepository formRepository)
         {
             _pathManager = pathManager;
+            _pluginManager = pluginManager;
             _formRepository = formRepository;
             _siteRepository = siteRepository;
         }
@@ -32,7 +35,8 @@ namespace SSCMS.Form.Core
         public async Task<string> ParseAsync(IParseStlContext context)
         {
             var formName = string.Empty;
-            var type = "submit1";
+            var type = string.Empty;
+            var height = string.Empty;
 
             foreach (var name in context.StlAttributes.AllKeys)
             {
@@ -46,6 +50,15 @@ namespace SSCMS.Form.Core
                 {
                     type = await context.ParseAsync(value);
                 }
+                else if (StringUtils.EqualsIgnoreCase(name, AttributeHeight))
+                {
+                    height = await context.ParseAsync(value);
+                }
+            }
+
+            if (string.IsNullOrEmpty(type))
+            {
+                type = "submit1";
             }
 
             var formInfo = !string.IsNullOrEmpty(formName) ? await _formRepository.GetFormInfoByTitleAsync(context.SiteId, formName) : null;
@@ -68,11 +81,23 @@ namespace SSCMS.Form.Core
                 var elementId = $"iframe_{StringUtils.GetShortGuid(false)}";
                 var libUrl = _pathManager.GetApiHostUrl(site, "assets/form/lib/iframe-resizer-3.6.3/iframeResizer.min.js");
                 var pageUrl = _pathManager.GetApiHostUrl(site, $"assets/form/templates/{type}/index.html?siteId={context.SiteId}&formId={formInfo.Id}&apiUrl={HttpUtility.UrlEncode(apiUrl)}");
-
-                return $@"
-<iframe id=""{elementId}"" frameborder=""0"" scrolling=""no"" src=""{pageUrl}"" style=""width: 1px;min-width: 100%;""></iframe>
+                var heightStyle = !string.IsNullOrEmpty(height) ? $"height: {height}" : string.Empty;
+                var frameResize = string.Empty;
+                if (!string.IsNullOrEmpty(height))
+                {
+                    heightStyle = $"height: {StringUtils.AddUnitIfNotExists(height)}";
+                }
+                else
+                {
+                    frameResize = $@"
 <script type=""text/javascript"" src=""{libUrl}""></script>
 <script type=""text/javascript"">iFrameResize({{log: false}}, '#{elementId}')</script>
+";
+                }
+
+                return $@"
+<iframe id=""{elementId}"" frameborder=""0"" scrolling=""no"" src=""{pageUrl}"" style=""width: 1px;min-width: 100%;{heightStyle}""></iframe>
+{frameResize}
 ";
             }
 
